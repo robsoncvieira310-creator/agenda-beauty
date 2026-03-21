5eed80071f0526090ec838a3f4cb61e266ff80cf
robsoncvieira310-creator <robsoncvieira310@gmail.com>
2026-03-20 22:54:09 -0300

    update

--- supabase/functions/create-profissional/index.ts
+++ supabase/functions/create-profissional/index.ts
@@ -7,7 +7,7 @@ const corsHeaders = {
 
 const supabaseUrl = Deno.env.get('SUPABASE_URL')!
 const supabaseServiceKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
-const supabaseAnonKey = Deno.env.get('SUPABASE_ANON_KEY')!
+const supabaseAnonKey = Deno.env.get('ANON_KEY')!
 
 Deno.serve(async (req) => {
   console.log('🔥 FUNÇÃO INICIADA')
